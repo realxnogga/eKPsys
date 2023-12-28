11a15a4d194243eca08a0c8571c1f8524e3a4212
Janzell <carl.oropesa11@gmail.com>
2023-12-28 14:58:17 +0800

    update report.php

--- report.php
+++ report.php
@@ -20,33 +20,128 @@ include 'report_handler.php';
 
     <title>Reports</title>
     <link rel="stylesheet" type="text/css" href="style copy.css">
-    <link href="path/to/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
 <style>
     .custom-modal-dialog {
     max-width: 800px; /* Adjust the width as needed */
     width: 90%; /* Adjust to your preference */
   }
+
+
+body, html {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+}
+
+.card {
+    height: 75vh; /* Set the height to 100% of the viewport height */
+    width: 160vh; /* Set the height to 100% of the viewport height */
+
+    overflow: auto;
+    padding-bottom: 20px; /* Add some padding to the bottom */
+    transition: height 0.3s ease; /* Add a smooth transition effect for height changes */
+}
+
+@media screen and (min-resolution: 192dpi), screen and (min-resolution: 2dppx) {
+    /* Adjust for high-density (Retina) displays */
+    .card {
+        height: 50vh;
+    }
+}
+
+@media screen and (max-width: 1200px) {
+    /* Adjust for window resolution 125% scaling */
+    .card {
+        height: 80vh;
+    }
+}
+
+@media screen and (max-width: 960px) {
+    /* Adjust for window resolution 150% scaling */
+    .card {
+        height: 66.67vh;
+    }
+}
+
+
+    .form-group {
+        margin-bottom: 1px;
+        }
+
+    .form-control-label {
+        font-weight: bold;
+    }
+
+        input[type="text"],
+        input[type="datetime-local"],
+        input[type="date"],
+        select {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+
+        input[type="submit"] {
+            background-color: green;
+            color: white;
+            padding: 10px 15px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: 0 auto; /* Center the submit button */
+    display: block; /* Ensure it takes up full width */
+        }
+
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+        /* Center align the submit button */
+        .row.justify-content-end {
+    display: flex;
+    justify-content: center;
+}
+
+.form-group.col-sm-2 {
+    text-align: center;
+    margin-right: 190px; /* Add some top margin for better spacing */
+}
+
 </style>
 <body>
-<hr><br>
-<body>
-    <div class="container">
+
+<div class="row">
+        <div class="leftcolumn">
+            <div class="card">
+                <div class="row">
+                <div class="container">
+
         <form method="POST">
             
-        <h2>Monthly Report (<?php echo isset($selected_month) ? $selected_month : date('F, Y'); ?>)</h2>
+        <h2>Annual Report (<?php echo isset($selected_year) ? $selected_year : date('F, Y'); ?>)</h2>
 
-          <!-- Dropdown to select month -->
-        <label for="selected_month">Select Month to Display Report:</label>
-        <select name="selected_month" id="selected_month">
-            <?php foreach ($months as $month) : ?>
-                <option value="<?php echo $month['month_year']; ?>"><?php echo $month['month_year']; ?></option>
-            <?php endforeach; ?>
-        </select>
-        <input type="submit" value="Select">
+<!-- Dropdown to select year -->
+<label for="selected_year">Select Year to Display Report:</label>
+<select name="selected_year" id="selected_year">
+    <?php foreach ($years as $year) : ?>
+        <option value="<?php echo $year['year']; ?>"><?php echo $year['year']; ?></option>
+    <?php endforeach; ?>
+</select>
+    <input type="submit" name="submit_annual" value="Select Annual Report">
+
+<h2>Monthly Report (<?php echo isset($selected_month) ? $selected_month : date('F, Y'); ?>)</h2>
+
+<!-- Dropdown to select month -->
+<label for="selected_month">Select Month to Display Report:</label>
+<select name="selected_month" id="selected_month">
+    <?php foreach ($months as $month) : ?>
+        <option value="<?php echo $month['month_year']; ?>"><?php echo $month['month_year']; ?></option>
+    <?php endforeach; ?>
+</select>
+    <input type="submit" name="submit_monthly" value="Select Monthly Report">
+
             <div class="row">
                 <div class="col-md-6">
                     <div class="form-group">
@@ -240,6 +335,12 @@ include 'report_handler.php';
       </div>
     </div>
   </div>
+</div>
+
+
+
+</div>
+</div>
 </div>
 
 

27633a4ad1908ee4c8fb2efa6b178bbd7054b8a2
angel02may <152768060+angel02may@users.noreply.github.com>
2024-02-07 09:52:49 +0800

    kp_form7
    7 lang to

--- formsT/kp_form7.php
+++ formsT/kp_form7.php
@@ -154,149 +154,157 @@ if (!empty($receivedDay) && !empty($receivedMonth) && !empty($receivedYear)) {
 </style>
 </head>
 <body>
-    <br>
     <div class="container">
-        <div class="paper">
-                <div class="top-right-buttons">
-                <!-- Print button -->
-                <button class="btn btn-primary print-button common-button" onclick="window.print()">
-                    <i class="fas fa-print button-icon"></i> Print
-                </button>
-                            <!-- Download button -->
-    <button class="btn btn-success download-button common-button" onclick="downloadPage()">
-            <i class="fas fa-download button-icon"></i> Download
-                </button>
-                <script>
-function downloadPage() {
-    var pageContent = document.documentElement.outerHTML;
-    var blob = new Blob([pageContent], { type: 'text/html' });
-    var url = URL.createObjectURL(blob);
-    var a = document.createElement('a');
-    a.href = url;
-    a.download = 'page.html';
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-    URL.revokeObjectURL(url);
-}
-</script>
-</body>
-                <a href="../manage_case.php?id=<?php echo $_SESSION['current_complaint_id']; ?>"><button class="btn common-button">
-                    <i class="button-icon"></i> Back
-                </button></a>
+    <div class="paper">
+    <div class="top-right-buttons">
+    <!-- Print button -->
+    <button class="btn btn-primary print-button common-button" onclick="window.print()" style="position: relative; right: 450px; top: -20px;">
+    <i class="fas fa-print button-icon"></i> Print
+    </button>
 
-            </div>
+    <a href="../manage_case.php?id=<?php echo $_SESSION['current_complaint_id']; ?>" style="position: relative; left: -1400px; top: -55px;">
+    <button class="btn common-button">
+        <i class="fas fa-arrow-left"></i> Back
+    </button>
+</a>
+</div>
+</body>
+<br>
+
+
+
+
             
-             <div style="text-align: left;">
-             <h5 style="font-size: 8px;">Pormularyo ng KP Blg. 7</h5>
-                <h5 style="text-align: center;">Republika ng Pilipinas</h5>
-                <h5 style="text-align: center;">Lalawigan ng Laguna</h5>
-                <h5 style="text-align: center;">Bayan ng <?php echo $_SESSION['municipality_name']; ?></h5>
-                <h5 style="text-align: center;">Barangay <?php echo $_SESSION['barangay_name']; ?></h5><br>
-                <h5 style="text-align: center;"><b>TANGGAPAN NG LUPONG TAGAPAMAYAPA</b></h5>
-            </div>
+            <div style="text-align: left; font-family: 'Times New Roman', Times, serif;">
+    <h5 style="font-size: 12px;">Pormularyo ng KP Blg. 7</h5>
+    <h5 style="text-align: center; font-size: 16px;">Republika ng Pilipinas</h5>
+    <h5 style="text-align: center; font-size: 16px;">Lalawigan ng Laguna</h5>
+    <h5 style="text-align: center; font-size: 16px;">Bayan ng <?php echo $_SESSION['municipality_name']; ?></h5>
+    <h5 style="text-align: center; font-size: 16px;">Barangay <?php echo $_SESSION['barangay_name']; ?></h5><br>
+    <h5 style="text-align: center; font-size: 16px; font-weight: bold;">TANGGAPAN NG LUPONG TAGAPAMAYAPA</h5>
+</div>
+
                 <?php
                 $months = [
-                    'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+                    'Enero', 'Pebrero', 'Marso', 'Abril', 'Mayo', 'Hunyo', 'Hulyo', 'Agosto', 'Setyembre', 'Oktubre', 'Nobyembre', 'Disyembre'];
 
                 $currentYear = date('Y');
                 ?>
    
                 
-  
-    <div class="form-group" style="text-align: right;">
+   <br>
+   <br>
+   
+                
+   <div class="form-group" style="text-align: justify; font-family: 'Times New Roman', Times, serif;">
+    <div class="input-field" style="float: right; width: 50%;">
+<!-- case num here -->
+<p style="text-align: left; font-size: 16px; margin-left: 30px; margin-bottom: 0;">
+    Usaping Barangay Blg. <span style="min-width: 178px; font-size: 16px; border-bottom: 1px solid black; display: inline-block;"><?php echo !empty($cNum) ? $cNum : '&nbsp;'; ?></span>
+</p>
+        <p style="text-align: left; font-size: 16px; font-size: 16px; margin-left: 30px; margin-top: 0;">
+        Ukol sa: <span style="border-bottom: 1px solid black; font-size: 16px;"><?php echo !empty($forTitle) ? nl2br(htmlspecialchars($forTitle)) : '&nbsp;'; ?></span>
 
-        <div class="input-field"> <br><br>
-            <!-- case num here -->
-            <div style="text-align: right;">  Usaping Barangay Blg.<?php echo $cNum; ?> </div>
-            <p> <div style="text-align: right;">Ukol sa : 
-            <!-- ForTitle here -->
-            <?php echo $forTitle; ?> <br> 
-        </div>
+</p>
+
     </div>
+</div>
 
-    <div class="form-group" style="text-align: justify; text-indent: 0em; margin-left: 20.5px;">
-        <div class="label"></div>
-        <div class="input-field">
-            <p> (Mga) Maysumbong	
-                <!-- CNames here -->
-                <br><?php echo $cNames; ?><br> </p>
-        <br><p>  — laban kay/kina —</p>
+<div class="form-group" style="text-align: justify; font-family: 'Times New Roman', Times, serif; text-indent: 0em; margin-left: 20.5px;">
+    <div class="label"></div>
+    <div class="input-field" style="font-size: 16px;">
+<!-- CNames here -->
+<div style="min-width: 250px; font-size: 16px; border-bottom: 1px solid black; display: inline-block;">
+    <?php echo !empty($cNames) ? $cNames : '&nbsp;'; ?>
+</div>
+<p style="font-size: 16px;">(Mga) Maysumbong</p>
+        <p style="font-size: 16px;">- laban kay/kina -</p>
     </div>
+</div>
+
+<div class="form-group" style="text-align: justify; font-family: 'Times New Roman', Times, serif; text-indent: 0em; margin-left: 20.5px;">
+    <div class="label"></div>
+    <div class="input-field;">
+<!-- RspndtNames here -->
+<div style="min-width: 250px; font-size: 16px; border-bottom: 1px solid black; display: inline-block;">
+    <?php echo !empty($rspndtNames) ? $rspndtNames : '&nbsp;'; ?>
+</div>
+<p style="font-size: 16px;"><i>(Mga) Ipinagsusumbong</i></p>
+
     </div>
+</div> 
 
-    <div>
-    <div class="form-group" style="text-align: justify; text-indent: 0em; margin-left: 20.5px;">
-        <div class="label"></div>
-        <div class="input-field">
-            <p> (Mga) Ipinagsusumbong<br>
-                <!-- RspndtNames here -->
-               <?php echo $rspndtNames; ?><br> </p>
-        </div>
-    </div>
+                    <h3 style="text-align: center; font-family: 'Times New Roman', Times, serif; font-size: 16px; font-weight: bold;">SUMBONG</h3>
 
-       
+                    <div style="text-align: justify; font-size: 16px; font-family: 'Times New Roman', Times, serif; text-indent: 0em; margin-left: 20.5px;"> 
+                    <p style="font-size: 16px;">AKO/KAMI, ay nagrereklamo laban sa mga ipinagsusumbong na binanggit sa itaas dahil sa paglabag ng aking/aming mga karapatan at kapakanan sa sumusunod na pamamaraan:
+                        <div class="a"><p style="text-align: left; font-size: 16px; font-size: 16px; margin-left: 2px; margin-top: 0;">
+       <span style="border-bottom: 1px solid black; font-size: 16px;"><?php echo !empty($cDesc) ? nl2br(htmlspecialchars($cDesc)) : '&nbsp;'; ?></span>
+                        
+                        
 
-                    <h3 style="text-align: center;"><b>SUMBONG</b></h3>
-
-                    <div style="text-align: justify; text-indent: 0em; margin-left: 20.5px;"> 
-                    <p>AKO/KAMI, ay nagrereklamo laban sa mga ipinagsusumbong na binanggit sa itaas dahil sa paglabag ng aking/aming mga karapatan at kapakanan sa sumusunod na pamamaraan: 
-                        <div class="a">
-  <textarea id="name" name="name" style="width: 700px; box-sizing: border-box; overflow-y: hidden;"><?php echo $cDesc; ?></textarea>
   <br>
 </div>
 
                 </p>
-                    <p>DAHIL DITO, AKO/KAMI, na nakikiusap na ipagkakaloob sa akin/amin ang sumusunod na (mga) kalunasan nang naaalinsunod sa batas at/o pagkamakatuwiran: <div class="a">
-  <textarea id="name" name="name" style="width: 700px; box-sizing: border-box; overflow-y: hidden;"><?php echo $petition; ?></textarea>
+                    <p style="font-size: 16px;">DAHIL DITO, AKO/KAMI, na nakikiusap na ipagkakaloob sa akin/amin ang sumusunod na (mga) kalunasan nang naaalinsunod sa batas at/o pagkamakatuwiran: <div class="a">
+  <span style="border-bottom: 1px solid black; font-size: 16px;"><?php echo !empty($petition) ? nl2br(htmlspecialchars($petition)) : '&nbsp;'; ?></span>
   <br>
 </div>
 
 <form id="formId" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    <div style="text-align: justify; text-indent: 0em; margin-left: 20.5px;">
-    Ginawa ngayong ika- 
-        <input type="number" name="made_day" placeholder="day" min="1" max="31" value="<?php echo isset($existingMadeDay) ? $existingMadeDay : ''; ?>">
-        araw ng 
-        <select name="made_month">
-            <option value="">Buwan</option>
-            <?php foreach ($months as $m): ?>
-                <option value="<?php echo $m; ?>" <?php echo isset($existingMadeMonth) && $existingMadeMonth === $m ? 'selected' : ''; ?>><?php echo $m; ?></option>
-            <?php endforeach; ?>
+    <div style="text-align: justify; font-family: 'Times New Roman', Times, serif; text-indent: 0em; font-size: 16px; margin-left: 20.5px; font-weight: bold">
+    <br>Ginawa ngayong ika- 
+    <span style="border-bottom: 1px solid black; font-size: 16px;"><?php echo !empty($existingMadeDay) ? nl2br(htmlspecialchars($existingMadeDay)) : '&nbsp;'; ?></span> araw ng 
+        <select name="made_month" style="font-size: 16px;">  
+    <option value="">Buwan</option>
+    <?php foreach ($months as $m): ?>
+        <option value="<?php echo $m; ?>" <?php echo isset($existingMadeMonth) && $existingMadeMonth === $m ? 'selected' : ''; ?>><?php echo $m; ?></option>
+    <?php endforeach; ?>
+</select>
         </select>,
-        <input type="number" name="made_year" placeholder="year" min="<?php echo date('Y') - 100; ?>" max="<?php echo date('Y'); ?>" value="<?php echo isset($existingMadeYear) ? $existingMadeYear : ''; ?>">
+        <input type="number" name="made_year" placeholder="year" min="<?php echo date('Y') - 100; ?>" max="<?php echo date('Y'); ?>" value="<?php echo isset($existingMadeYear) ? $existingMadeYear : ''; ?>" style="font-size: 16px; width: auto; display: inline-block;">
+<span style="font-size: 16px; display: inline-block; margin-left: -38px;">.</span>
+
     </div>
     <div style="position: relative;">
         <br>
-        <p class="important-warning-text" style="text-align: center; font-size: 12px; margin-left: 570px; margin-right: auto;" >
-            <!-- CName here but All Capital Letters --><br><br><br><p class="important-warning-text" style="text-align: center; font-size: 12px; margin-left: 570px; margin-right: auto;">
-    <?php echo $cNames; ?><br>
-     _________________
-    <label id="cmplnts" name="cmplnts" size="25" style="text-align: center;">(mga) Maysumbong</label>
-</p>
+        <p class="important-warning-text" style="text-align: center; font-family: 'Times New Roman', Times, serif; font-size: 24px; margin-left: 570px; margin-right: auto;" >
+        <div style="text-align: right;">
+        <div style="min-width: 250px; border-bottom: 1px solid black; display: inline-block; text-align: center; font-size: 16px; position: relative;">
+    <?php echo $cNames; ?>
+    <br> <!-- Add a line break to move the next element to a new line -->
+    <label id="cmplnts" name="cmplnts" size="25" style="text-align: center; font-size: 16px; position: absolute; top: 50%; left: 0; right: 0; transform: translateY(-50%); font-weight: normal;"><br><br>(mga) Maysumbong</label>
+    </div>
+</div></p></span>
 
     </div>
-    <div style="text-align: justify; text-indent: 0em; margin-left: 20.5px;">
+    <br><br><div style="text-align: justify; font-family: 'Times New Roman', Times, serif; text-indent: 0em; font-size: 16px; margin-left: 20.5px;">
     Tinanggap at inihain ngayong  
-<input type="number" name="received_day" placeholder="day" min="1" max="31" value="<?php echo isset($existingReceivedDay) ? $existingReceivedDay : ''; ?>">
+    <span style="border-bottom: 1px solid black; font-size: 16px;"><?php echo !empty($existingReceivedDay) ? nl2br(htmlspecialchars($existingReceivedDay)) : '&nbsp;'; ?></span>
 araw ng 
-        <select name="received_month">
-            <option value="">Buwan</option>
-            <?php foreach ($months as $m): ?>
-                <option value="<?php echo $m; ?>" <?php echo isset($existingReceivedMonth) && $existingReceivedMonth === $m ? 'selected' : ''; ?>><?php echo $m; ?></option>
-            <?php endforeach; ?>
+<select name="received_month" style="font-size: 16px;">
+    <option value="">Buwan</option>
+    <?php foreach ($months as $m): ?>
+        <span style="border-bottom: 1px solid black; font-size: 16px;"><?php echo !empty($existingReceivedMonth) ? nl2br(htmlspecialchars($existingReceivedDay)) : '&nbsp;'; ?></span>        <option value="<?php echo $m; ?>" <?php echo isset($existingReceivedMonth) && $existingReceivedMonth === $m ? 'selected' : ''; ?>><?php echo $m; ?></option>
+    <?php endforeach; ?>
+</select>
         </select>,
-        <input type="number" name="received_year" placeholder="year" min="<?php echo date('Y') - 100; ?>" max="<?php echo date('Y'); ?>" value="<?php echo isset($existingReceivedYear) ? $existingReceivedYear : ''; ?>">
+        <input type="number" name="received_year" placeholder="year" min="<?php echo date('Y') - 100; ?>" max="<?php echo date('Y'); ?>" value="<?php echo isset($existingReceivedYear) ? $existingReceivedYear : ''; ?>" style="font-size: 16px;">
+        <span style="font-size: 16px; display: inline-block; margin-left: -38px;">.</span>
     </div>
-    <?php if (!empty($message)) : ?>
-        <p><?php echo $message; ?></p>
-    <?php endif; ?>
-    <input type="submit" name="saveForm" value="Save" class="btn btn-primary print-button common-button">
 </form>
 <br>
 <br>
-                    <p class="important-warning-text" style="text-align: center; font-size: 12px; margin-left: 570px; margin-right: auto;"><?php echo $punong_barangay; ?><br>_________________<br>
-                    <label id="punongbrgy" name="punongbrgy" size="25" style="text-align: center;">Punong Barangay/Kalihim ng Lupon</label>
-</p>
+<div style="text-align: right;">
+    <div style="min-width: 250px; border-bottom: 1px solid black; display: inline-block; text-align: center; font-size: 16px;">
+        <?php echo $punong_barangay; ?>
+    </div>
+    <br> <!-- Add a line break to move the next element to a new line -->
+    <label id="punongbrgy" name="punongbrgy" size="25" style="text-align: center; min-width: 250px; font-family: 'Times New Roman', Times, serif; font-size: 16px; font-weight: normal;"><i>Punong Barangay/Kalihim ng Lupon</i></label>
+
+</div>
+
                 </div>
             </div>
         </div> <br>
